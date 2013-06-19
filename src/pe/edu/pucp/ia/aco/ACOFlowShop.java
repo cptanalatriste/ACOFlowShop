1a9cd5f3002ab28b73f5cff593da9671289ebd35
ccamaleon5 <adriancc5.5@gmail.com>
2013-06-19 01:05:25 -0400

    Update ACOFlowShop.java

--- src/pe/edu/pucp/ia/aco/ACOFlowShop.java
+++ src/pe/edu/pucp/ia/aco/ACOFlowShop.java
@@ -141,7 +141,7 @@ public class ACOFlowShop {
 				int j = 0;
 				for (String s : split) {
 					if (!s.isEmpty()) {
-						graph[i - 1][j++] = Integer.parseInt(s) + 1;
+						graph[i - 1][j++] = Integer.parseInt(s);
 					}
 				}
 			} else {
